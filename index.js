9fe7c834db35d81098b074b6caa6ce8a242fcf21
nabil13090 <nabil.chetouane13090@gmail.com>
2023-12-05 15:53:23 +0100

    modif

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ let nav = document.getElementById("nav");
 icon.addEventListener("click", () =>{
     nav.classList.toggle("active");
 })
+
+let alertShow = false;
+
+setInterval(() => {
+    document.title =
+    alertShow ? "bienvenue dans le site"
+                : "Naruto";
+
+        alertShow = !alertShow;
+}, 1000);
